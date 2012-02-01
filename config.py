85e504cacecb938a760cbc16f35f96022c4a5fb1
bigeagle <justin.w.xd@gmail.com>
2012-02-01 23:17:01 +0800

    official retweet

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ ACCESS_SECRET = 'se9ztHd3GiuYm5bYXfVdeGQhUkiJ2MVHn7EryqXuE0'
 #手动发Tweet的KEY
 TWEET_KEY = 'V8Q6DvexTyCE3se47rtBX8em'
 ## 关键词
-RT_REGEX="""xdtuxbot|linux|unix|android|meego|ubuntu|arch|gentoo|debian|firefox|chrome|chromium|python|geek|apache|nginx|sql|php|django|rails|ruby|GPL|vim|emacs|gnome|gnu|perl|freebsd|netbsd|openbsd|hack|html5| kde """
+RT_REGEX="""xdtuxbot|linux|unix|android|meego|ubuntu|arch|gentoo|debian|firefox|chrome|chromium|python| geek |apache|nginx|sql|php|django|rails|ruby|GPL|vim|emacs|gnome|gnu|perl|freebsd|netbsd|openbsd|hack|html5| kde """
 
-MGC="gfw|翻墙|中共|土共|共产党|当局|政府|#notrt"
+MGC="gfw|翻墙|中共|土共|共产党|当局|政府|#notrt|#nort"
 ## 访问路径
 URL_RT = '/RT'                      # 推群
 URL_TIMELINE = '/timeline'          # 时间线
